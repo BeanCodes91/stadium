33274e09a18c772f0e11125041d37115336bd25e
Melanie Knorn <Melanie.Knorn.91@gmail.com>
2022-06-30 09:57:20 +0200

    Create hibernate.cfg.xml and DBAccess class

---
+++ src/main/java/dba/DBAccess.java
@@ -0,0 +1,32 @@
+package dba;
+
+import org.hibernate.*;
+import org.hibernate.boot.*;
+import org.hibernate.boot.registry.*;
+//import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class DBAccess {
+
+	static Session session;
+	static Transaction transaction;
+
+	public static void main(String[] args) {
+		//Configuration c = new Configuration().configure();
+		StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+		Metadata md = new MetadataSources(registry).getMetadataBuilder().build();
+		SessionFactory sf = md.getSessionFactoryBuilder().build();
+		session = sf.openSession();
+		transaction = session.beginTransaction();
+//		session.persist(new SportsEvent());
+//		session.persist(new EventBooking());	
+		transaction.commit();
+	}
+
+	public static void createEntities() {
+	
+	}
+}
